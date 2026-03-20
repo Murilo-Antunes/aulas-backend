4b911ec0232372a6dc2d4c09ceeb07674bb0fed0
MURILO ANTUNES CAMPOS DA ROCHA <25203706@cfp127aluno.local>
2026-03-20 08:35:11 -0300

    modificando comentarios do codigo

--- Exercicio 05/module/manipular_array.js
+++ Exercicio 05/module/manipular_array.js
@@ -3,38 +3,15 @@
 * Data: 16/03/2026  
 * Autor: Murilo
 * **********************************************************************/
-/* 
-Criar uma função (getListaDeEstados) que retorna a lista de todos os
-estados do Brasil.
-
-Criar uma função (getDadosEstado) que retorna as informações referente
-a um estado do Brasil, onde a sigla do estado será o critério de filtro.
-
-Criar uma função (getCapitalEstado) que retorna as informações referente
-a capital de um estado do Brasil, onde a sigla do estado será o critério de
-filtro.
-
-Criar uma função (getEstadosRegiao) que retorna as informações
-referente aos estados do Brasil conforme a sua região, onde a região será o
-critério de filtro.
-
-Criar uma função (getCapitalPais) que retorna as informações referente
-aos estados que formam a capital do Brasil.
-
-
-Criar uma função (getCidades) que retorna uma lista de cidades, filtrado
-pela sigla do estado.
-
-https://drive.google.com/drive/folders/16pVimM4AqFNkS74zX4QKlXvEFGVdQzHw
-*/
 
 const json = require('./estados_cidades')
 
+//retorna a lista de todos os estados do Brasil.
 const getListaDeEstados = function(){
     let sigla = []
     let estado = false
-    json.listaDeEstados.estados.forEach(function(estado){
-        sigla.push(estado.sigla)
+    json.listaDeEstados.estados.forEach(function(uf){
+        sigla.push(uf.sigla)
     })
     estado = {
         'UF' : sigla,
@@ -44,6 +21,7 @@ const getListaDeEstados = function(){
     return estado
 }
 
+//que retorna as informações referente a um estado do Brasil, onde a sigla do estado será o critério de filtro.
 const getDadosEstado = function(sigla){
     let siglaRecebida = sigla
     let estado = false
@@ -60,7 +38,7 @@ const getDadosEstado = function(sigla){
     return estado
 }
 
-
+//retorna as informações referente a capital de um estado do Brasil, onde a sigla do estado será o critério de filtro.
 const getCapitalEstado = function(sigla){
     let siglaRecebida = sigla
     let capital = false
@@ -76,6 +54,7 @@ const getCapitalEstado = function(sigla){
     return capital
 }
 
+//retorna as informações referente aos estados do Brasil conforme a sua região, onde a região será o critério de filtro.
 const getEstadosRegiao = function(regiao){
     let regiaoRecebida = regiao
     let informacoesRegiao = false
@@ -94,6 +73,7 @@ const getEstadosRegiao = function(regiao){
     return informacoesRegiao
 }
 
+//retorna as informações referente aos estados que formam a capital do Brasil.
 const getCapitalPais = function(){
     let objeto = false
     let arrayEstados = []
@@ -116,6 +96,7 @@ const getCapitalPais = function(){
     return objeto
 }
 
+//retorna uma lista de cidades, filtrado pela sigla do estado.
 const getCidades = function(sigla){
     let siglaRecebida = sigla
     let arrCidades = []
@@ -136,5 +117,5 @@ const getCidades = function(sigla){
     })
     return estado
 }
-console.log(getListaDeEstados('RJ'))
+
 
