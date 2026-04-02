07105e8b260a7f978c540d64d7514395f1aad31b
MURILO ANTUNES CAMPOS DA ROCHA <25203706@cfp127aluno.local>
2026-04-02 10:55:06 -0300

    criando validações para as rotas

--- Exercicio 05/module/manipular_array.js
+++ Exercicio 05/module/manipular_array.js
@@ -118,4 +118,5 @@ const getCidades = function(sigla){
     return estado
 }
 
+console.log(getListaDeEstados())
 
--- aula 06/app.js
+++ aula 06/app.js
@@ -44,6 +44,83 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
+
+
+
+app.get('/v1/senai/estados/dados/:sigla', function(request, response){
+    let sigla = request.params.sigla
+    let funcao = getDadosEstado(sigla)
+
+    if(!funcao){
+        response.status(404)
+        response.json({"message": "erro na url"})
+    }else{
+        response.status(200)
+        response.json(funcao)
+    }
+
+    
+})
+
+app.get('/v1/senai/estados/capital/:sigla', function(request, response){
+    let sigla = request.params.sigla
+    let funcao = getCapitalEstado(sigla)
+
+    if(!funcao){
+        response.status(404)
+        response.json({"message": "erro na url"})
+    }else{
+        response.status(200)
+        response.json(funcao)   
+    }
+
+    
+})
+
+app.get('/v1/senai/estados/regiao/:regiao', function(request, response){
+    let regiao = request.params.regiao
+    let funcao = getEstadosRegiao(regiao)
+
+    if(!funcao){
+        response.status(404)
+        response.json({"message": "erro na url"})
+    }else{
+        response.status(200)
+        response.json(funcao)   
+    }
+
+    
+})
+
+app.get('/v1/senai/capital/pais', function(request, response){
+    let funcao = getCapitalPais()
+
+    if(!funcao){
+        response.status(404)
+        response.json({"message": "erro na url"})
+    }else{
+        response.status(200)
+        response.json(funcao)
+    }
+
+    
+})
+
+app.get('/v1/senai/cidades/:sigla', function(request, response){
+    let sigla = request.params.sigla
+    let funcao = getCidades(sigla)
+
+    if(!funcao){
+        response.status(404)
+        response.json({"message": "erro na url"})
+    }else{
+        response.status(200)
+        response.json(funcao) 
+    }
+
+    
+})
+
 //criando endpoints para API
     // o "/estados" é a assinatura do endpoint
     // o endpoint sempre trabalha com response(retornos da api | res) e request(chegadas de dados na api | req)
@@ -55,84 +132,47 @@ app.get('/v1/senai/estados', function(request, response){
     response.status(200) //status "200" que representa um sucesso de requisição de uma API
 }) 
 
-
-app.get('/v1/senai/estados/dados/:sigla', function(request, response){
-    let sigla = request.params.sigla
-    let funcao = getDadosEstado(sigla)
-
-    if(!funcao){
-        response.json({"message": "erro na url"})
-        response.status(404)
+app.get('/v1/senai/help', function(request, response){
+    const docApi = {
+        "API-descripition"  : "API para manipular dados de Estados e Cidades",
+        "development"     : "Murilo Antunes",
+        "date"              : "2026-04-02",
+        "version"         : "1.0.0",
+        "endpoints": [
+            {
+                "id": 1,
+                "Rota 1" : "http://localhost:8080/v1/senai/estados",
+                "obs" : "retorna a lista de todos estados"
+            },
+            {
+                "id": 2,
+                "Rota 2" : "http://localhost:8080/v1/senai/estados/dados/sp",
+                "obs" : "retorna a lista de dados de um estado de acordo com a sigla"
+            },   
+            {
+                "id": 3,
+                "Rota 3" : "http://localhost:8080/v1/senai/estados/capital/sp",
+                "obs" : "retorna os dados da capital filtrando pela sigla do estado"
+            },   
+            {
+                "id": 4,
+                "Rota 4" : "http://localhost:8080/v1/senai/estados/regiao/sul",
+                "obs" : "retorna todos os estados referente a uma região"
+            },
+            {
+                "id": 5,
+                "Rota 5" : "http://localhost:8080/v1/senai/capital/pais",
+                "obs" : "retorna todos os estados que já foram a capital do país"
+            },
+            {
+                "id": 6,
+                "Rota 6" : "http://localhost:8080/v1/senai/cidades/{sigla}",
+                "obs" : "retorna todas cidades filtrando pela sigla do estado"
+            }                  
+        ]
     }
 
-    response.json(funcao)
-    response.status(200)
-})
-
-app.get('/v1/senai/estados/capital/:sigla', function(request, response){
-    let sigla = request.params.sigla
-    let funcao = getCapitalEstado(sigla)
-
-    if(!funcao){
-        response.json({"message": "erro na url"})
-        response.status(404)
-    }
-
-    response.json(funcao)
-    response.status(200)
-})
-
-app.get('/v1/senai/estados/regiao/:sigla', function(request, response){
-    let sigla = request.params.sigla
-    let funcao = getEstadosRegiao(sigla)
-
-    if(!funcao){
-        response.json({"message": "erro na url"})
-        response.status(404)
-    }
-
-    response.json(funcao)
-    response.status(200)
-})
-
-app.get('/v1/senai/capital/pais', function(request, response){
-    let funcao = getCapitalPais()
-
-    if(!funcao){
-        response.json({"message": "erro na url"})
-        response.status(404)
-    }
-
-    response.json(funcao)
-    response.status(200)
-})
-
-app.get('/v1/senai/cidades/:sigla', function(request, response){
-    let sigla = request.params.sigla
-    let funcao = getCidades(sigla)
-
-    if(!funcao){
-        response.json({"message": "erro na url"})
-        response.status(404)
-    }
-
-    response.json(funcao)
-    response.status(200)
-})
-
-app.get('/', function(request, response){
-    response.json({
-        "autor: "       : "Murilo Antunes",
-        "versão: "      : "1.0.0",
-        "endpoints: "   : [
-                            "http://localhost:8080/v1/senai/estados",
-                            "http://localhost:8080/v1/senai/estados/dados/{sigla}",
-                            "http://localhost:8080/v1/senai/estados/capital/{sigla}",
-                            "http://localhost:8080/v1/senai/estados/regiao/{sigla}",
-                            "http://localhost:8080/v1/senai/capital/pais",
-                            "http://localhost:8080/v1/senai/cidades/{sigla}"
-                        ]
-    })
+    response.json(docApi)
 })
 
 
